cea8f6fefd6303c7f2993de9fe2d552f878fabeb
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2017-01-18 19:24:38 -0500

    update README

--- nforce-server/config/database.js
+++
@@ -1,8 +0,0 @@
-/**
-* @license:
-* MIT (c) 2016 Gordon m. Finnie III */
-
-module.exports = {
-  'secret' : 'wowzers',
-  'database': '********YOUR DATABASE IP*******'
-};
--- nforce-server/config/fields.js
+++
@@ -1,7 +0,0 @@
-/**
-* @license:
-* MIT (c) 2016 Gordon m. Finnie III */
-module.exports = {
-
-
-};
